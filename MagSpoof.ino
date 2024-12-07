635c18643ea2f82dbc56b703bf4ceb1dfcdbfe9b
KaiyangYao <yaoky2008@gmail.com>
2024-12-07 06:05:40 -0500

    change to only spoof track 2 data

--- MagSpoof.ino
+++ MagSpoof.ino
@@ -6,32 +6,24 @@
 #define ENABLE_PIN  13     // L293D Enable pin for the coil (EN1,2)
 #define BUTTON_PIN  2      // Button input for starting playback
 #define CLOCK_US    400    // Delay for bit transmission, in microseconds
-#define LEADING_ZEROS 40   // Number of leading zeros for synchronization
-#define TRAILING_ZEROS 40  // Number of trailing zeros for synchronization
+#define LEADING_ZEROS 25   // Number of leading zeros for synchronization
+#define TRAILING_ZEROS 25  // Number of trailing zeros for synchronization
 
-// Track Data
-const char* tracks[] = {
-  // "%B4503111122223333^MIAOSKI LIN              ^2010200999909999999900990000000?", // Track 1
-  // ";4503111122223333=200120099999999?"                                            // Track 2
-  
+// Track2 Data Options
+const char* track2s[] = {
   ";6009550002147835?",  // Anoop 1
   ";6009550001931114?",  // Alex 2 
   ";6009550002492900?",  // Korgan 3 
-  ";6009550002372300?"  // Kaiyang 4 
-
-
-
-
+  ";6009550002372300?"   // Kaiyang 4 
 };
 
-// const int sublen[] = {32, 48};  // ASCII offsets for Track 1 and Track 2
-// const int bitlen[] = {7, 5};    // Bits per character for Track 1 and Track 2
-const int sublen[] = {48, 48, 48, 48};  // ASCII offsets for Track 1 and Track 2
-const int bitlen[] = {5, 5, 5, 5};    // Bits per character for Track 1 and Track 2
+const int numTrack2s = sizeof(track2s) / sizeof(track2s[0]);
+const int sublen = 48;  // ASCII offsets for Track 2
+const int bitlen = 5;   // Bits per character for Track 2
 
-volatile bool playTrackFlag = false; // Flag to indicate track playback
-int polarity = 0;                    // Coil direction toggle
-unsigned int curTrack = 0;           // Current track index (alternates between Track 1 and Track 2)
+volatile int currentTrack2Index = 0;
+
+int polarity = 0;       // Coil direction toggle
 
 // Set polarity for the coil
 void setPolarity(int polarity) {
@@ -59,12 +51,11 @@ void playBit(int bit) {
 }
 
 // Play a full track, including leading/trailing zeros and error checking
-void playTrack(int track) {
+void playTrack2(const char* trackData) {
   int tmp, crc, lrc = 0;
-  track--; // Convert track to zero-based index
 
-  Serial.print("Playing Track ");
-  Serial.println(track + 1);
+  Serial.println("Playing Track 2 data:");
+  Serial.println(trackData);
 
   // Enable the H-bridge
   digitalWrite(ENABLE_PIN, HIGH);
@@ -75,11 +66,10 @@ void playTrack(int track) {
   }
 
   // Send the track data
-  for (int i = 0; tracks[track][i] != '\0'; i++) {
+  for (int i = 0; trackData[i] != '\0'; i++) {
     crc = 1; // Start with odd parity
-    tmp = tracks[track][i] - sublen[track]; // Adjust ASCII offset
-
-    for (int j = 0; j < bitlen[track] - 1; j++) {
+    tmp = trackData[i] - sublen; // Adjust ASCII offsets
+    for (int j = 0; j < bitlen - 1; j++) {
       crc ^= tmp & 1;        // Calculate parity
       lrc ^= (tmp & 1) << j; // Update LRC
       playBit(tmp & 1);
@@ -91,7 +81,7 @@ void playTrack(int track) {
   // Send LRC (Longitudinal Redundancy Check)
   tmp = lrc;
   crc = 1;
-  for (int j = 0; j < bitlen[track] - 1; j++) {
+  for (int j = 0; j < bitlen - 1; j++) {
     crc ^= tmp & 1;
     playBit(tmp & 1);
     tmp >>= 1;
@@ -123,10 +113,11 @@ void loop() {
   if (digitalRead(BUTTON_PIN) == LOW) { // Button pressed
     delay(50); // Debounce
     if (digitalRead(BUTTON_PIN) == LOW) { // Confirm button press
-      Serial.println("Button pressed. Playing track.");
-      playTrack(curTrack + 1); // Play the current track (1-based index)
-      Serial.println(sizeof(tracks));
-      curTrack = (curTrack + 1) % 4; // Alternate between Track 1 and Track 2
+      Serial.print("Button pressed. Spoofing Track 2 data index: ");
+      Serial.println(currentTrack2Index);
+      
+      playTrack2(track2s[currentTrack2Index]);
+      currentTrack2Index = (currentTrack2Index + 1) % numTrack2s;
     }
     while (digitalRead(BUTTON_PIN) == LOW); // Wait for button release
   }
